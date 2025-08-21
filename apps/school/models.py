5b97a830b00843299063f360cd129c1cfd84bb5e
Ali Hassan <owerflow.d@gmail.com>
2025-08-21 13:44:03 +0500

    add email & password field & validations for password

--- apps/school/models.py
+++ apps/school/models.py
@@ -1,25 +1,32 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+from django.contrib.auth.hashers import make_password, check_password
 
 from apps.common.enums import SchoolTypeEnum
-
 
 class School(models.Model):
     name = models.CharField(max_length=100)
     unique_id = models.SlugField(max_length=50, unique=True, db_index=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
     address = models.TextField(null=True, blank=True)
     city = models.CharField(max_length=50, null=True, blank=True)
     state = models.CharField(max_length=50, null=True, blank=True)
     zipcode = models.CharField(max_length=20, null=True, blank=True)
     contact_number = models.CharField(max_length=20, null=True, blank=True)
-    email = models.EmailField(null=True, blank=True)
 
     subscription_start_date = models.DateField(null=True, blank=True)
     subscription_end_date = models.DateField(null=True, blank=True)
-
     established_year = models.PositiveIntegerField(null=True, blank=True)
     principal_name = models.CharField(max_length=100, null=True, blank=True)
+
+    # Add these fields for better security
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     school_type = models.CharField(
         max_length=20,
@@ -27,6 +34,22 @@ class School(models.Model):
         null=True,
         blank=True
     )
+
+    def set_password(self, raw_password):
+        """Hash and set the password"""
+        self.password = make_password(raw_password)
+
+
+    def check_password(self, raw_password):
+        """Check if the provided password matches the stored hash"""
+        return check_password(raw_password, self.password)
+
+
+    def save(self, *args, **kwargs):
+        # Hash password if it's not already hashed
+        if self.password and not self.password.startswith(('pbkdf2_', 'bcrypt', 'argon2')):
+            self.set_password(self.password)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name} ({self.unique_id})"
