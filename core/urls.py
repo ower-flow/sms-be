4ee3d4f4efddfdf1ec9ffe5080c12238915feae2
Elie Hacen <owerflow.d@gmail.com>
2025-09-29 00:45:04 +0500

    add school & teacher login routes

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf.urls.static import static
-
-from django.views.generic import TemplateView
 
 
 urlpatterns = [
    # path('admin/', admin.site.urls, name="admin"),
-   path('api/v1/school/', include('apps.school.urls', namespace='school')),
+    path("api/v1/school/",  include("apps.school.urls",  namespace="school")),
+    path("api/v1/teacher/", include("apps.teacher.urls", namespace="teacher")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
